7d171f942ab66588bffe7010d7faf85aff4403ab
MGonza20 <sara.paguaga@gmail.com>
2023-02-22 00:25:08 -0600

    Creando métodos para crear autómata

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 
 
+from thompsonTools.Bridge import Bridge
+
 class Compilador:
     def __init__(self, regex):
         self.regex = regex
         self.sims = {'(': 1, '|': 2, '.': 3, '*': 4, '+': 4, '?': 4}
         self.statesNo = 0
-
+        self.transitions = {}
 
     def concat(self):
         newRegex, ops = "", list(self.sims.keys())
@@ -25,10 +27,8 @@ class Compilador:
         newRegex += self.regex[-1]
         return newRegex
 
-
     def prec(self, value):
         return 5 if value.isalnum() else self.sims[value]
-
 
     def infixPostfix(self):
         postfix, stack = '', []
@@ -47,14 +47,121 @@ class Compilador:
                 while stack and self.prec(stack[-1]) >= self.prec(value):
                     postfix += stack.pop()
                 stack.append(value)
-                
+
         while stack:
             postfix += stack.pop()
         return postfix
 
 
+    def MYT(self):
+        stringg = self.infixPostfix()
+        stack = []
+
+        for value in stringg:
+            if value.isalnum(): 
+                # Definiendo estados iniciales y finales
+                start = self.statesNo
+                end = self.statesNo + 1
+                
+                # Generando transicion
+                transitions = {}
+                transitions[self.statesNo] = {value: [self.statesNo+1]}
+                
+                stack.append(Bridge(start, end, transitions))
+                self.statesNo += 2
+
+            elif value == '.':
+                # Obteniendo los dos ultimos elementos de la pila
+                el2 = stack.pop()
+                el1 = stack.pop()
+                el1.trs.update(el2.trs)
+
+                # Generando transicion
+                el1.trs[el1.end] = {'ε': [el2.start]}
+
+                # Creando nuevo estado
+                stack.append(Bridge(el1.start, el2.end, el1.trs))
+
+            elif value == '|':
+                # Obteniendo los dos ultimos elementos de la pila
+                el2 = stack.pop()
+                el1 = stack.pop()
+                el1.trs.update(el2.trs)
+
+                # Generando transicion
+                start = self.statesNo
+                end = self.statesNo + 1
+                el1.trs[start] = {'ε': [el1.start, el2.start]}
+                el1.trs[el1.end] = {'ε': [end]}
+                el1.trs[el2.end] = {'ε': [end]}
+
+                # Creando nuevo estado
+                stack.append(Bridge(start, end, el1.trs))
+                self.statesNo += 2
+
+            elif value == '*':
+                # Obteniendo el ultimo elemento de la pila
+                el1 = stack.pop()
+
+                # Generando transicion
+                start = self.statesNo
+                self.statesNo += 1
+
+                end = self.statesNo + 1
+                self.statesNo += 1
+
+                el1.trs[start] = {'ε': [el1.start, end]}
+                el1.trs[el1.end] = {'ε': [el1.start, end]}
+
+                # Creando nuevo transicion
+                stack.append(Bridge(start, end, el1.trs))
+
+            elif value == '+':
+                # Obteniendo el ultimo elemento de la pila
+                el1 = stack.pop()
+
+                # Generando transicion
+                start = self.statesNo
+                end = self.statesNo + 1
+                el1.trs[start] = {'ε': [el1.start]}
+                el1.trs[el1.end] = {'ε': [el1.start, end]}
+
+                # Creando nuevo estado
+                stack.append(Bridge(start, end, el1.trs))
+                self.statesNo += 2
+            
+            elif value == '?':
+                start = self.statesNo
+                end = self.statesNo + 1
+                    
+                # Generando transicion
+                transitions = {}
+                transitions[self.statesNo] = {'ε': [self.statesNo+1]}
+                self.statesNo += 2
+                
+                el1 = stack.pop()
+                el2 = Bridge(start, end, transitions)
+                el1.trs.update(el2.trs)
+
+                # Generando transicion
+                start = self.statesNo
+                end = self.statesNo + 1
+                el1.trs[start] = {'ε': [el1.start, el2.start]}
+                el1.trs[el1.end] = {'ε': [end]}
+                el1.trs[el2.end] = {'ε': [end]}
+
+                # Creando nuevo estado
+                stack.append(Bridge(start, end, el1.trs))
+                self.statesNo += 2
+                
+
+
+        return stack.pop().trs
 
 
 
-compi = Compilador("0?(1?)?0*") 
-print(compi.infixPostfix())
+
+
+# compi = Compilador("0?(1?)?0*") 
+compi = Compilador("a?(b?)")
+print(compi.MYT())
--- thompsonTools/Bridge.py
+++ thompsonTools/Bridge.py
@@ -1,8 +1,9 @@
 
 # Description: Esta clase es usada para representar una transicion
 # entre dos estados en un AFN.
-class Bridge():
-    def __init__(self, start, final, transition):
-        self.start = start
-        self.final = final
-        self.transition = transition
+class Bridge:
+    def __init__(self, start, end, trs):
+        self.start = start 
+        self.end = end
+        self.trs = trs
+        
