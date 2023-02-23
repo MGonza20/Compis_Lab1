ff2bce102f314d1334cea39d195572a9c3ff50f9
MGonza20 <sara.paguaga@gmail.com>
2023-02-23 02:55:18 -0600

    Comentarios

--- thompsonTools/Format.py
+++ thompsonTools/Format.py
@@ -3,28 +3,40 @@ class Format:
     def __init__(self, regex):
         self.regex = regex
         self.sims = {'(': 1, '|': 2, '.': 3, '*': 4, '+': 4, '?': 4}
+
+
+    def prec(self, value):
+        return 5 if value.isalnum() else self.sims[value]
         
+
     def concat(self):
         newRegex, ops = "", list(self.sims.keys())
         ops.remove('(')
 
         for i in range(len(self.regex)):
+            # Caracter actual es val
             val = self.regex[i]
             if i+1 < len(self.regex):
+                # Caracter siguiente es val_p1
                 val_p1 = self.regex[i+1]
                 newRegex += val
 
-                if val != '(' and val_p1 != ')' \
-                   and val != '|' \
-                   and val_p1 not in ops:
+                # Validacion No. 1
+                # Si el operador actual no es un parentesis que abre
+                # y el siguiente no es un parentesis que cierra
+                
+                # Validacion No. 2
+                # Si el carater actual no es un operador binario 
+                # o no contiene al caracter de la izquierda
+
+                # Validacion No. 3
+                # Si los operadores no contienen al caracter de la derecha
+
+                if val != '(' and val_p1 != ')' and val != '|' and val_p1 not in ops:
                     newRegex += '.'
 
         newRegex += self.regex[-1]
         return newRegex
-
-
-    def prec(self, value):
-        return 5 if value.isalnum() else self.sims[value]
 
 
     def infixPostfix(self):
@@ -41,7 +53,7 @@ class Format:
                 stack.pop()
 
             else:
-                while stack and self.prec(stack[-1]) >= self.prec(value):
+                while stack and self.prec(value) <= self.prec(stack[-1]):
                     postfix += stack.pop()
                 stack.append(value)
 
